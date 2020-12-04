83a3b2c0f3bb0e0ee04818ed18ba8be9d545bf4d
Kamilla Sani <kamilla@courrier.agency>
2020-12-04 20:53:31 +0100

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 
 def post_list(request):
@@ -14,9 +15,18 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, "blog/post_detail.html", {"post": post})
+    return redirect("post_detail", pk=post.pk)
 
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return render(request, "blog/post_edit.html", {"form": form})
+    else:
+        form = PostForm()
     return render(request, "blog/post_edit.html", {"form": form})
