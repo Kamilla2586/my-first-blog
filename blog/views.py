64dc88a8b5bf5b328400d76dda94a0d2c499a89c
Kamilla Sani <kamilla@courrier.agency>
2020-12-05 16:14:48 +0100

    Add a new view for post_edit

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return redirect("post_detail", pk=post.pk)
+    return render(request, "blog/post_detail.html", {"post": post})
 
 
 def post_new(request):
@@ -26,7 +26,22 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return render(request, "blog/post_edit.html", {"form": form})
+            return redirect("post_detail", pk=post.pk)
     else:
         form = PostForm()
+    return render(request, "blog/post_edit.html", {"form": form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, "blog/post_edit.html", {"form": form})
