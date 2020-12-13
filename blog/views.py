bb43f1bf6c92c0b38db02de4a2c3b591d4a3ee1e
Kamilla Sani <kamilla@courrier.agency>
2020-12-13 21:05:18 +0100

    Add login restrictions

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
@@ -18,6 +19,7 @@ def post_detail(request, pk):
     return render(request, "blog/post_detail.html", {"post": post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -32,6 +34,7 @@ def post_new(request):
     return render(request, "blog/post_edit.html", {"form": form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
